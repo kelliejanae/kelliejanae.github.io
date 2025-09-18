3339233e76cf9143f2c5ef45a455f0ab76e1db0e
Kellie Stephens <kelliejanae@gmail.com>
2025-09-17 22:01:55 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,32 @@ topBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
+//Swiper JS
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  speed: 300,
+  autoplay: {
+    delay: 9000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+// Read more click
+document.querySelectorAll('.swiper-slide').forEach(slide => {
+  slide.style.cursor = 'pointer';
+  slide.addEventListener('click', () => {
+    const url = slide.getAttribute('data-readmore');
+    if (url) window.open(url, '_blank');
+  });
+});
