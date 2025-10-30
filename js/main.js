59af46d68bb903df2ce940246317f45b01fa17b0
Kellie Stephens <kelliejanae@gmail.com>
2025-10-29 21:18:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,44 +1,23 @@
-// Get button
-const topBtn = document.getElementById("topBtn");
+/* inspiration from
+https://cz.pinterest.com/pin/830703093796717544/
+*/
 
-// Show button after scrolling down
-window.onscroll = function() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    topBtn.style.display = "block";
-  } else {
-    topBtn.style.display = "none";
+gsap.registerPlugin(SplitText);
+
+let split = SplitText.create(".text", {
+  type: "chars, words, lines",
+  mask: "lines"
+});
+
+gsap.from(split.chars, {
+  yPercent: "random([-100, 100])",
+  rotation: "random(-30, 30)",
+  ease: "back.out",
+  autoAlpha: 0,
+  repeat: 4,
+  yoyo: true,
+  stagger: {
+    amount: 0.5,
+    from: "random"
   }
-};
-
-// Scroll smoothly to top
-topBtn.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
 });
-
-$(".custom-carousel").owlCarousel({
-  autoWidth: true,
-  loop: true
-});
-
-$(document).ready(function () {
-  // Step A: Set all items to their default background on load
-  $(".custom-carousel .item").each(function () {
-    $(this).css("background-image", "url('" + $(this).data("default") + "')");
-  });
-
-  // Step B: When clicking an item...
-  $(".custom-carousel .item").click(function () {
-    // Reset all to default backgrounds
-    $(".custom-carousel .item").each(function () {
-      $(this)
-        .removeClass("active")
-        .css("background-image", "url('" + $(this).data("default") + "')");
-    });
-
-    // Set this one to its active background
-    $(this)
-      .addClass("active")
-      .css("background-image", "url('" + $(this).data("active") + "')");
-  });
-});
-
